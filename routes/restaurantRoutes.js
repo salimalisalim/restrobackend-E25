370056be921046f53f197d0102e53f5783a150f9
salimalisalim <46714650+salimalisalim@users.noreply.github.com>
2024-03-21 15:32:39 +0530

    Update restaurantRoutes.js

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const { authToken } = require("../middleware/auth");
 const upload = require("../middleware/fileUpload");
-const { addRestaurant, getRestaurants } = require("../controllers/restaurantController");
+const { addRestaurant, getRestaurants, getSampleData } = require("../controllers/restaurantController");
 
 
 const router = express.Router();
 
 router.route('/add').post(authToken, upload.single('photograph'), addRestaurant);
 router.route('/restaurants').get(getRestaurants);
+router.route('/sample').get(getSampleData);
 
 
 module.exports = router;
